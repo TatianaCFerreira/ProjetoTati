1e9cc4cf527199264f6e5dadbf5635a530db1803
TatianaCFerreira <taticferreira.x@gmail.com>
2023-10-27 16:42:11 -0300

    initial commit

--- script.js
+++ script.js
@@ -1,14 +1,11 @@
 function toggleMode() {
   const html = document.documentElement
-  html.classList.toggle('light')
+  html.classList.toggle("light")
 
   const img = document.querySelector("#profile img")
-  if (html.classList.contains('light')) {
-  img.setAttribute('src', './assets/assets/avatar eu-sun.png')
-  } else { 
+  if (html.classList.contains("light")) {
+    img.setAttribute("src", "./assets/assets/avatar eu-sun.png")
+  } else {
     img.setAttribute("src", "./assets/assets/avatar eu.png")
   }
-
-
-  
 }
